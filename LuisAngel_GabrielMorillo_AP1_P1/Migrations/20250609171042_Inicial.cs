1a92e46fa06ecf522c54edea55315418c751a411
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2025-06-09 19:11:32 +0200

    Exportando la migracion.

---
+++ LuisAngel_GabrielMorillo_AP1_P1/Migrations/20250609171042_Inicial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuisAngel_GabrielMorillo_AP1_P1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aportes",
+                columns: table => new
+                {
+                    AportesId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fecha = table.Column<DateOnly>(type: "date", nullable: false),
+                    Monto = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aportes", x => x.AportesId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aportes");
+        }
+    }
+}
