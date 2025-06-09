77e5dbf6af78c4a72c23594d05fd5e923d715c29
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2025-06-09 19:07:58 +0200

    Agregando los parametros al model.

---
+++ LuisAngel_GabrielMorillo_AP1_P1/Models/Aportes.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuisAngel_GabrielMorillo_AP1_P1.Models
+{
+    public class Aportes
+    {
+        [Key]
+        public int AportesId { get; set; }
+        
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public string Nombres { get; set; } = string.Empty;
+        public DateOnly Fecha { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+       [Range(1, double.MaxValue, ErrorMessage = "El monto tiene que ser mayor de 1")]
+       public double monto { get; set; }
+
+    }
+}
