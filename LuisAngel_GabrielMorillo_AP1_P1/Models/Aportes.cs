8cc6fa7105f9844c69bcbcc57bf7f54d45611c4a
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2025-06-09 19:09:08 +0200

    Corrigiendo la palabra monto.

--- LuisAngel_GabrielMorillo_AP1_P1/Models/Aportes.cs
+++ LuisAngel_GabrielMorillo_AP1_P1/Models/Aportes.cs
@@ -12,7 +12,7 @@ namespace LuisAngel_GabrielMorillo_AP1_P1.Models
         public DateOnly Fecha { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
        [Range(1, double.MaxValue, ErrorMessage = "El monto tiene que ser mayor de 1")]
-       public double monto { get; set; }
+       public double Monto { get; set; }
 
     }
 }
